28c9e37010d60f40b8783f39db62941fa45ab6e3
edwinndungu <edwin.ndungu2@student.moringaschool.com>
2025-03-10 14:35:31 +0300

    complete labs

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+//multiplying two numbers
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2    
+console.log(multiply)
+
+
+//generates a random integer greater than 0
+const random = Math.floor(Math.random() * 10) + 1
+console.log(random) 
+
+//calculates a remainder that is equal to 4
+const num3 = 16
+const num4 = 6
+const mod = num3 % num4
+console.log(mod)    
+
+//finds the highest number in a set
+const max = Math.max(1, 2, 20, 19)
+console.log(max)
+
 
 // Write your code here
